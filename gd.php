4fb421709ecfe33b604f335ef6563f7483c00326
Nishantkr2203 <nishantkr9065@gmail.com>
2024-09-26 14:37:14 +0530

    Add files via upload

---
+++ gd.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<link href="file:///C|/Users/India/Desktop/test/css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	
+</body>
+</html>
